cfa4c687243e190a8e95b63f13d77fc29a42fc66
Piyushnmj <piyushnmj@yahoo.com>
2022-10-17 11:23:27 +0530

    UC10: Employee Wage for Multiple Companies

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -2,12 +2,10 @@
 {
     static void Main(string[] args)
     {
-        EmpWageBuilderObject Microsoft = new EmpWageBuilderObject("Microsoft", 100, 20, 100);
-        EmpWageBuilderObject Google = new EmpWageBuilderObject("Google", 200, 30, 150);
-        Microsoft.computeEmpWage();
-        Console.WriteLine(Microsoft.toString());
-        Google.computeEmpWage();
-        Console.WriteLine(Google.toString());
+        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        empWageBuilder.addCompanyEmpWage("Microsoft", 100, 20, 100);
+        empWageBuilder.addCompanyEmpWage("Google", 200, 30, 150);
+        empWageBuilder.computeEmpWage();
         Console.ReadLine();
     }
 }
