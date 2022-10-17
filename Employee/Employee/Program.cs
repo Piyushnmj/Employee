ef5aa98be3cbf0925a66956fd49626f08e212fae
Piyushnmj <piyushnmj@yahoo.com>
2022-10-17 10:23:10 +0530

    UC1: Employee is present or absent

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,8 +1,20 @@
-﻿internal class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Welcome!");
+        //Constants
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        //Computation
+        int empCheck = random.Next(0, 2);
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
         Console.ReadLine();
     }
 }
