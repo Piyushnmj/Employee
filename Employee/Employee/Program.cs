01c92ae2f7c35f6574fcbf1c16b9bb5c6dab272d
Piyushnmj <piyushnmj@yahoo.com>
2022-10-20 18:35:47 +0530

    UC14: user input method

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -1,22 +1,28 @@
-﻿class Program
+﻿//UC12: List of multiple companies to manage Employee Wage.
+//UC13: Daily Wage along with Total Wage
+//UC14: Ability to get the Total Wage when queried by company.
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a Company Name: ");
         string companyname = Console.ReadLine();
 
-        Console.WriteLine("Enter Employee Rate Per Hour: ");
+        Console.WriteLine("\nEnter Employee Rate Per Hour: ");
         int empRatePerHour = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter number of working days: ");
+        Console.WriteLine("\nEnter number of working days: ");
         int numOfWorkingDays = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("Enter max hours per month: ");
+        Console.WriteLine("\nEnter max hours per month: ");
         int maxHoursPerMonth = Convert.ToInt32(Console.ReadLine());
 
-        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
         empWageBuilder.addCompanyEmpWage(companyname, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
         empWageBuilder.computeEmpWage();
+
+        Console.WriteLine("\nTotal Wage when queried by the companies is as follows: ");
+        Console.WriteLine("\nTotal wage for " + companyname + " is " + empWageBuilder.getTotalWage(companyname));
         Console.ReadLine();
     }
 }
